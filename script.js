115a945bc208373e480f1f4785ddf36b50c7eae9
asadbek001019-a11y <asadbek001019@gmail.com>
2026-01-23 10:20:39 +0330

    hello

--- script.js
+++
@@ -1,8 +0,0 @@
-const shoe = document.getElementById("shoe");
-const shine = document.querySelector(".shine");
-
-shoe.addEventListener("click", () => {
-    shine.classList.remove("active");
-    void shine.offsetWidth; // перезапуск анимации
-    shine.classList.add("active");
-});
